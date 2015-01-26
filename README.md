18139db763de56bbfa51503054c881b85fded28a
Claudio Petrini <claudio.petrini@stamplay.com>
2015-01-26 22:18:58 +0100

    Added README, several fixes

---
+++ README.md
@@ -0,0 +1,12 @@
+# nginx + spdy + pagespeed #
+
+Bash script that help to compile nginx with:
+- a parametric nginx version
+- a parametric openssl version   
+- a parametric pagespeed module version
+
+Spdy protocol supported by default. 
+
+## Binary ##
+
+The compiled binary right now is placed in the `output` folder and will be created at the same level of the install script.
--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
 #bin/bash
 
-#Versions
+#nginx version
 NGINX_VERSION=1.7.9
+#openssl version
 OPENSSL_VERSION=1.0.1l
+#ngx_pagespeed version
 NPS_VERSION=1.9.32.3
 
 #Directories
 CURRENT_DIR=$(pwd)
-OPENSSL_DIR=$(pwd)/openssl-$OPENSSL_VERSION
+OPENSSL_DIR=$(pwd)/openssl-${OPENSSL_VERSIO}N
 
 #Save the nginx version
-curl -O http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
+curl -O http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 #Extracting nginx 
-tar -xvf nginx-$NGINX_VERSION.tar.gz
+tar -xvf nginx-${NGINX_VERSION}.tar.gz
 #Save the openssl version
-curl -O https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
+curl -O https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 #Extracting openssl
-tar -xvf openssl-$OPENSSL_VERSION.tar.gz
+tar -xvf openssl-${OPENSSL_VERSION}.tar.gz
 #Save ngx_pagespeed module
 wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip
 #Extracting ngx_pagespeed module
@@ -26,26 +28,26 @@ cd ngx_pagespeed-release-${NPS_VERSION}-beta/
 #Downloading psol
 wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
 #Extracting psol
-tar -xzvf ${NPS_VERSION}.tar.gz  # extracts to psol/
+tar -xzvf ${NPS_VERSION}.tar.gz 
 # Exiting from ngx_pagespeed
 cd ..
 
 #Entering inside the nginx folder
-cd nginx-$NGINX_VERSION
+cd nginx-${NGINX_VERSION}
 #Configuring 
 ./configure  \
---prefix=$CURRENT_DIR/output \
+--prefix=${CURRENT_DIR}/output \
 --with-http_ssl_module \
 --with-http_spdy_module \
---with-openssl=$OPENSSL_DIR \
+--with-openssl=${OPENSSL_DIR} \
 --with-cc-opt="-Wno-deprecated-declarations" \
 --add-module=${CURRENT_DIR}/ngx_pagespeed-release-${NPS_VERSION}-beta
 
-#For compiling OpenSsl on MacOsX, compiling everything
+# Compiling everything on 64 bits systems
 KERNEL_BITS=64 make install
 
 #Cleaning sources
-cd $CURRENT_DIR
+cd ${CURRENT_DIR}
 rm *.tar.gz
-rm -rf nginx-$NGINX_VERSION OPENSSL_DIR
+rm -rf nginx-${NGINX_VERSION} ${OPENSSL_DIR}
 
