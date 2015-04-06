d6f8febe93204787291045ee1dc1c1946562d431
Claudio Petrini <claudio.petrini@stamplay.com>
2015-04-06 16:19:19 +0200

    Updated OpenSSL version

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 sudo apt-get install unzip zlibc zlib1g build-essential zlib1g-dev libpcre3 libpcre3-dev libssl-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev
 
 #nginx version
-NGINX_VERSION=1.7.9
+NGINX_VERSION=1.7.11
 
 #openssl version
-OPENSSL_VERSION=1.0.1l
+OPENSSL_VERSION=1.0.2a
 
 #ngx_pagespeed version
 NPS_VERSION=1.9.32.3
