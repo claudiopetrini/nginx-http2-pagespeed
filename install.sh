45ae95d14a778bedbe01fb7d7c3c9ea162e087e7
Claudio Petrini <claudio.petrini@stamplay.com>
2016-03-02 08:45:04 +0100

    Bumped to OpenSSL 1.0.2g and nginx 1.9.12

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 sudo apt-get install unzip zlibc zlib1g build-essential zlib1g-dev libpcre3 libpcre3-dev libssl-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev curl
 
 #nginx version
-NGINX_VERSION=1.9.7
+NGINX_VERSION=1.9.12
 
 #openssl version
-OPENSSL_VERSION=1.0.2d
+OPENSSL_VERSION=1.0.2g
 
 #ngx_pagespeed version
 NPS_VERSION=1.9.32.3
